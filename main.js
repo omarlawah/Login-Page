411aa31789dfc47f77a26fdf4d553f0ea6ed9578
omarlawah <omarxxxxx135@gmail.com>
2023-05-06 14:38:15 +0000

    styling the validated password inputs in javascript file

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ let span = document.querySelector('#error-msg');
 function validatePass(){
     if (confirmPassword.value !== password.value){
         span.innerHTML='*Both password input fields should<br>have the same value';
+        password.style.borderColor='red';
+        confirmPassword.style.borderColor='red';
     }else{
         span.textContent='';
+        password.style.borderColor='green';
+        confirmPassword.style.borderColor='green';
     }
 }
 
-
-
 password.addEventListener('input', validatePass);
 confirmPassword.addEventListener('input', validatePass);
+
